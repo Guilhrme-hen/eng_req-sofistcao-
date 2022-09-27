8d9923e5a6c90e4bcd23cfb63426bab6d27a7b8d
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2022-09-27 16:11:00 -0300

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -3,7 +3,7 @@
  
  main(){
  char nomeusuario[50];
- double senha,confirmacao;
+ double senha,confirmacao,valormerc;
  int opcao,cpffuncionario,cpfcliente,codmerc,qntmerc,cpfcliente1;
  char nomefuncionario[50],nomecliente[50],nomemerc[50],ServoProd,endfuncionario[60],endcliente[60],mercpedido[60],pagamento[50];
  
@@ -18,15 +18,11 @@ scanf ("%i" ,&confirmacao);
 
 if (senha == confirmacao){
 
-	printf ("\n\n\t\tseja bem vindo ao sistema Sofistcao sr %s",nomeusuario);
-}
-else 
-{
-	printf ("\nsenhas nao conferem\n\nporfavor reinicie o programa e tente novamente");
-}
+	printf ("\n\n\t\tseja bem vindo ao sistema Sofistcao sr %s\n",nomeusuario);
+
 
  
- printf (" Escolha uma opcao...\n");	
+ printf ("\nEscolha uma opcao...\n\n");	
  printf ("[1] cadastro de funcionario\n");	
  printf ("[2] cadastro de cliente\n");	
  printf ("[3] cadastro de mercadoria\n");	
@@ -40,74 +36,85 @@ else
 	 {
  		case 1:
  		system ("cls");
-		printf ("------cadasto de funcionario------\n");
-		printf ("digite o nome do funcionario: ");
+		printf ("------Cadasto de funcionario------\n\n");
+		printf ("Digite o nome do funcionario: ");
 		scanf ("%s" ,&nomefuncionario);
 		printf ("\n");
-		printf ("informe o cpf: ");
+		printf ("Informe o cpf: ");
  		scanf ("%i" ,&cpffuncionario);
  		printf ("\n");
  		fflush(stdin);
-		printf ("informe o endereco: ");
+		printf ("Informe o endereco: ");
  		scanf ("%[^\n]s" ,&endfuncionario);
 		printf ("\n\n");
-printf ("funcionario %s cadastrado com sucesso\n", nomefuncionario);
+printf ("Funcionario %s cadastrado com sucesso\n\n", nomefuncionario);
 		system ("pause");
 
 	 case 2:
  		system ("cls");
-		printf ("\n\n------cadasto de cliente-----\n");
-		printf ("digite o nome do cliente: ");
+		printf ("------Cadasto de cliente-----\n\n");
+		printf ("Digite o nome do cliente: ");
 		scanf ("%s" ,&nomecliente);
 		printf ("\n");
 		fflush(stdin);
-		printf ("informe o cpf: ");
+		printf ("Informe o cpf: ");
 		scanf ("%i" ,&cpfcliente);
  		printf ("\n");
  		fflush(stdin);
-		printf ("informe o endereco: ");
+		printf ("Informe o endereco: ");
  		scanf ("%s" ,&endcliente);
  		printf ("\n");
  		fflush(stdin);
-printf ("cliente %s cadastrado com sucesso\n", nomecliente); 
+printf ("Cliente %s cadastrado com sucesso\n\n", nomecliente); 
 		system ("pause");
 
 	case 3:
 		system ("cls");
-		printf ("\n\n------cadasto de mercadoria-----\n\n");
-		printf ("digite o nome da mercadoria: ");
-		scanf ("%s" ,&nomemerc);
-		printf ("\n");
+		printf ("-----Cadasto de mercadoria-----\n\n");
 		fflush(stdin);
-		printf ("informe o cod da mercadoria: ");
+		printf ("Informe o cod da mercadoria: ");
  		scanf ("%i" ,&codmerc);
  		printf ("\n");
  		fflush(stdin);
-		printf ("informe a quantidade: ");
+		printf ("Digite o nome da mercadoria: ");
+		scanf ("%s" ,&nomemerc);
+		printf ("\n");
+		printf ("Informe o valor da mercadoria mercadoria: ");
+		scanf ("%lf.2" ,&valormerc);
+		printf ("\n");
+		printf ("Informe a quantidade: ");
  		scanf ("%i" ,&qntmerc);
 	 	printf ("\n");
 	 	fflush(stdin);
-		printf ("informe [P] para produto e [S] para servico: ");
+		printf ("Informe [P] para produto e [S] para servico: ");
  		scanf ("%c" ,&ServoProd);
  		printf ("\n");
  		fflush(stdin);
-printf ("item %s cadastrado com sucesso\n",nomemerc); 
+printf ("Item %s cadastrado com sucesso\n\n",nomemerc); 
 		system ("pause");
 	
 	case 4:
-		printf ("\n------pedido-----\n");
-		printf ("digite o nome da mercadoria: ");
+		system ("cls");
+		printf ("\n------Pedido-----\n\n");
+		printf ("Digite o nome da mercadoria: ");
 		scanf ("%s", &mercpedido);
 		printf ("\n");
-		printf ("digite o cpf do cliente: ");
+		printf ("Digite o cpf do cliente: ");
 		scanf ("%i", &cpfcliente1);
 		fflush(stdin);
 		printf ("\n"); 
-		printf ("digite a forma de pagamento entre cartao\ndinheiro\npix\n");
+		printf ("Digite a forma de pagamento entre\ncartao\ndinheiro\npix\n");
 		scanf("%s", &pagamento);
-		printf ("opicao escolida foi %s",pagamento);
+		printf ("Opicao escolida foi %s\n\n",pagamento);
+		system ("pause");
 }
 system ("cls");
 printf ("------ordem de servico/pedido------\n");
-printf ("nome do atendente %s\nnome do cliente %s\nservico/produto %s forma de pagamento",nomefuncionario,nomecliente,mercpedido,pagamento);
+printf ("Nome do atendente: %s\nNome do cliente: %s\nServico/produto: %s\nPreço da compra: %.2lf\nforma de pagamento %s",nomefuncionario,nomecliente,mercpedido,valormerc,pagamento);
  }
+
+else 
+{
+	printf ("\nsenhas nao conferem\n\nporfavor reinicie o programa e tente novamente");
+}
+}
