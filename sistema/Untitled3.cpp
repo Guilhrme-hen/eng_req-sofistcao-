0b6b2ab04c25363b283a12c4473f672376944d85
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2022-09-27 12:02:17 -0300

    Add files via upload

---
+++ sistema/Untitled3.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+char nomeusuario[50];
+double senha,confirmacao;
+
+printf ("----------------cadastro de usuario----------------\n\n\ndigite o nome de usuario: ");
+scanf ("%s" ,&nomeusuario);
+	
+printf ("\ndigite a senha usando somente numeros para cadastra o usuario: ");
+scanf ("%i" ,&senha);
+
+printf ("\nconfirme a senha de cadastro: ");
+scanf ("%i" ,&confirmacao);
+
+if (senha == confirmacao){
+
+	printf ("\n\n\t\tseja bem vindo ao sistema Sofistcao sr %s",nomeusuario);
+}
+else 
+{
+	printf ("\nsenhas nao conferem\n\nporfavor reinicie o programa e tente novamente");
+}
+
+}
